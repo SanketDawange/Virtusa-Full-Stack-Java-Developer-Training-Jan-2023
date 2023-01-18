44682342372248681eac31ee6ab595eb3b996b72
SanketDawange <sanketdawange6@gmail.com>
2023-01-18 11:54:50 +0530

    Updated

--- Java/Overriding and Overloading/MethodOverriding.java
+++ Java/Overriding and Overloading/MethodOverriding.java
@@ -1,5 +1,18 @@
+interface A{
+    public void printName();
+}
+
+class B implements A{
+    @Override
+    public void printName() {
+        System.out.println("Name is Sanket");
+    }
+}
+
 public class MethodOverriding {
     public static void main(String[] args) {
-        
+        System.out.println("Method overridding in JAVA");
+        B b = new B();
+        b.printName();
     }
 }
